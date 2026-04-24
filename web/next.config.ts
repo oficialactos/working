10f52690fababab77e29573cd01b5615f5cb7f7b
gabrsoaress <gabrielfrs34@gmail.com>
2026-04-24 11:05:46 -0300

    Atualização instantânea

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   images: {
@@ -15,6 +16,13 @@ const nextConfig: NextConfig = {
   },
 };
 
-
-
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  skipWaiting: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})(nextConfig);
