ca2734a30849a20313cda84706f0988d36726aff
gabrsoaress <gabrielfrs34@gmail.com>
2026-04-25 10:50:46 -0300

    Correção de erros

--- web/src/app/dashboard/client/new/page.tsx
+++ web/src/app/dashboard/client/new/page.tsx
@@ -1015,22 +1015,3 @@ export default function NewRequestPage() {
   );
 }
 
-function PlusCircle({ size, className }: { size: number, className?: string }) {
-  return (
-    <svg 
-      width={size} 
-      height={size} 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2.5" 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-      className={className}
-    >
-      <circle cx="12" cy="12" r="10" />
-      <line x1="12" y1="8" x2="12" y2="16" />
-      <line x1="8" y1="12" x2="16" y2="12" />
-    </svg>
-  );
-}
